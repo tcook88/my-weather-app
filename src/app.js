1d73293495dd37cad4e9254c4302e52b02694241
tcook88 <104636723+tcook88@users.noreply.github.com>
2022-10-07 11:22:34 -0400

    update time

--- src/app.js
+++ src/app.js
@@ -1,28 +1,26 @@
-let now = new Date();
 
-let hours = now.getHours();
-if(hours < 10) {
-    hours = `0${hours}`;
+
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if(hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday", 
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"];
+    let day = days[date.getDay()];
+    return `Last updated: ${day} ${hours}:${minutes}`;
 }
-
-let minutes = now.getMinutes();
-if (minutes < 10) {
-    minutes = `0${minutes}`;
-}
-
-let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday", 
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"];
-let day = days[now.getDay()];
-
-let currentTime = document.querySelector(".current-time");
-
-currentTime.innerHTML = `Last updated: ${day} ${hours}:${minutes}`;
 
 
 function showTemp(response) {
@@ -32,11 +30,13 @@ function showTemp(response) {
     let description = document.querySelector("#description");
     let humidity = document.querySelector("#humidity");
     let wind = document.querySelector("#wind");
+    let currentTime = document.querySelector("#date");
     currentTemp.innerHTML = Math.round(response.data.main.temp);
     city.innerHTML = response.data.name;
     description.innerHTML = response.data.weather[0].description;
     humidity.innerHTML = response.data.main.humidity;
     wind.innerHTML = Math.round(response.data.wind.speed);
+    currentTime.innerHTML = formatDate(response.data.dt * 1000);
 }
 
     let city = "New York";
